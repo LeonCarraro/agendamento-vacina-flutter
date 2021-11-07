0b1b39dca037149fb2dedf02a69bec10130a5179
Leonardo Carraro <lcarraro.oliveira@gmail.com>
2021-11-07 13:23:58 -0300

    finalização da página de escolha de horários

--- lib/pages/schedule_page.dart
+++ lib/pages/schedule_page.dart
@@ -30,54 +30,123 @@ class _SchedulePageState extends State<SchedulePage> {
       backgroundColor: AppColor.homePageBackground,
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.only(top: 25, left: 25, right: 25,),
           child: Column(
             children: [
-              SizedBox(height: 30,),
-              CustomWidget.Back(title: "Voltar", onTap: () => {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => HealthPostsPage()))
-              }),
-              SizedBox(height: 30,),
-              CustomWidget.Step(title: "Passo 04 de 04"),
-              SizedBox(height: 10,),
-              CustomWidget.PageTitle(title: "Escolha o horário da vacinação"),
-              SizedBox(height: 30,),
-              
-              SizedBox(height: 10,),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  FutureBuilder(
-                    future: findSchedules(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (_, index) {
-                            return CustomWidget.GroupCard(
-                              title: snapshot.data[index]["dayOfWeek"] + "  -  " + snapshot.data[index]["dayOfMonth"],
-                              description: snapshot.data[index]["schedule"],
-                              extraLine: false,
-                              nextPage: null,
-                            );
-                          },
-                        );
-                      } else {
-                        return Column(
-                          children: [
-                            SizedBox(height: 50,),
-                            Center(
-                              child: CircularProgressIndicator()
-                            )
-                          ]
-                        );
-                      }
-                    }
-                  ),
-                ]
+              Container(
+                padding: EdgeInsets.only(top: 25, left: 25, right: 25,),
+                child: Column(
+                  children: [
+                    SizedBox(height: 30,),
+                    CustomWidget.Back(title: "Voltar", onTap: () => {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => HealthPostsPage()))
+                    }),
+                    SizedBox(height: 30,),
+                    CustomWidget.Step(title: "Passo 04 de 04"),
+                    SizedBox(height: 10,),
+                    CustomWidget.PageTitle(title: "Escolha o horário da vacinação"),
+                    SizedBox(height: 30,),
+                  ],
+                )
               ),
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.only(left: 20, top: 40, right: 20, bottom: 40),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            AppColor.gradientFirst.withOpacity(0.8),
+                            AppColor.gradientSecond.withOpacity(0.9)
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0)
+                        )
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Local selecionado",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColor.homePageContainerTextSmall,
+                              fontWeight: FontWeight.w300
+                            ),
+                          ),
+                          SizedBox(height: 15,),
+                          Row(
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 10, right: 10)),
+                              Icon(
+                                Icons.location_on_outlined,
+                                size: 40,
+                                color: AppColor.homePageContainerTextSmall,
+                              ),
+                              Padding(padding: EdgeInsets.only(left: 10, right: 10)),
+                              Flexible(
+                                child: Text(
+                                  "U.B.S Morro Azul \"Eliza Bulgarelli Buzetto\"",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: AppColor.homePageContainerTextSmall
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 0, left: 25, right: 25,),
+                child: Column(
+                  children: [
+                    SizedBox(height: 10,),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        FutureBuilder(
+                          future: findSchedules(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return ListView.builder(
+                                shrinkWrap: true,
+                                physics: NeverScrollableScrollPhysics(),
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (_, index) {
+                                  return CustomWidget.GroupCard(
+                                    title: snapshot.data[index]["dayOfWeek"] + "  -  " + snapshot.data[index]["dayOfMonth"],
+                                    description: snapshot.data[index]["schedule"],
+                                    extraLine: false,
+                                    nextPage: null,
+                                  );
+                                },
+                              );
+                            } else {
+                              return Column(
+                                children: [
+                                  SizedBox(height: 50,),
+                                  Center(
+                                    child: CircularProgressIndicator()
+                                  )
+                                ]
+                              );
+                            }
+                          }
+                        ),
+                      ]
+                    ),
+                  ],
+                )
+              )
             ]
           )
         )
